edc2d5832d036ec09940911aff8ef77ede010bcf
Tsimur Asayonak <t.asaynok@gmail.com>
2025-07-06 14:15:44 +0200

    Init project

---
+++ StoriesApp/StoriesApp/StoriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesAppApp.swift
+//  StoriesApp
+//
+//  Created by Valentyna Chemer on 06/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
